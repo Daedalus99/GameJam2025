7acff93ee9e51eea5ea91dc4ecd36e80352fd456
Daedalus99 <locke.harper@gmail.com>
2025-10-13 23:00:55 -0400

    Going to save literal decades of my life play testing now that conveyor speed is better :)

--- Scripts/partSpawner.gd
+++ Scripts/partSpawner.gd
@@ -5,7 +5,7 @@ extends Node3D
 @export var conveyor_speed := 1.0
 @export var spawn_interval := 3.0
 @export var work_zone: Area3D   # assign the Area3D in inspector
-
+@export var rotationRandomness : float = PI/15.0
 @onready var timer: Timer = $SpawnTimer
 @onready var belt: CSGBox3D = $Belt
 
@@ -27,7 +27,7 @@ func _ready() -> void:
 		belt_mat = ShaderMaterial.new()
 		belt_mat.shader = SCROLL_SHADER
 	belt.material = belt_mat
-	belt_mat.set_shader_parameter("scroll_speed", speed_uv_per_sec)
+	belt_mat.set_shader_parameter("scroll_speed", speed_uv_per_sec*conveyor_speed)
 	belt_mat.set_shader_parameter("phase", phase)
 
 	# spawn timer
@@ -51,6 +51,7 @@ func _process(delta: float) -> void:
 
 func _on_timer_timeout() -> void:
 	var corpse := scene_to_spawn.instantiate()
+	corpse.visuals.rotation.y = randf_range(-rotationRandomness, rotationRandomness) + (randi_range(0,1)*PI)
 	path.add_child(corpse)
 	# expect corpse script to have .speed and .set_drive()
 	corpse.call("set_drive", moving)
